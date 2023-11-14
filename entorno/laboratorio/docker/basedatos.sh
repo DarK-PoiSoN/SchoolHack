206ec1496a123b9f5a60fabd09fd9ce1fa351998
David <65144190+DarK-PoiSoN@users.noreply.github.com>
2023-11-14 21:54:06 +0100

    Update basedatos.sh
    Añadimos la creación de la red

--- entorno/laboratorio/docker/basedatos.sh
+++ entorno/laboratorio/docker/basedatos.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+docker network create --subnet=172.18.0.0/16 sql
 docker run -p 3306:3306 --net sql --ip 172.18.0.2 --name basedatos -h basedatossql -d davicillo12/basedatos:v1
 sleep 2
 docker exec basedatos cp -r /backup/mysql/ /var/lib/
+sleep 1
 docker restart basedatos
+sleep 1
